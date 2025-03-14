ec073b0c485708f7e1496aaf0ffed3fc59d6a345
PeculiarVictor <160975802+PeculiarVictor@users.noreply.github.com>
2025-03-14 18:56:19 +0000

    Add files via upload

---
+++ List.py
@@ -0,0 +1,17 @@
+#Create an empty list
+empty_list = []
+print()
+
+
+# A list of numbers 
+numbers = [1, 2, 3, 3, 4, 5]
+print(numbers)
+
+
+#use * operator
+triples = [1, 2, 3] * 3
+print(triples)
+#reserve the given list
+alist= [100, 200, 300, 400, 500]
+alist = alist[::-1]
+print(alist,"\n")
